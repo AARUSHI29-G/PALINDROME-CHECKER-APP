cbc570c7770a87880bc50724b5a957f5f8cf7434
AARUSHI29-G <aarushigullia@gmail.com>
2026-02-26 15:53:18 +0530

    UC1: Add welcome message module

--- src/UseCase1PalindromeCheckerApp.java
+++ src/UseCase1PalindromeCheckerApp.java
@@ -26,7 +26,7 @@ public class UseCase1PalindromeCheckerApp {
 
     /**
      * Application entry point.
-     *
+     * <p>
      * This is the first method executed by the JVM
      * when the program starts.
      *
@@ -38,39 +38,8 @@ public class UseCase1PalindromeCheckerApp {
         System.out.println("Version : 1.0");
         System.out.println("System initialized successfully.");
     }
-    // Hardcoded string
-    String input = "madam";
-
-    boolean isPalindrome = true;
-
-    // Loop only till half of the string length
-        for (int i = 0; i < input.length() / 2; i++) {
-
-        // Compare characters from both ends
-        if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
-            isPalindrome = false;
-            break;
-        }
-    }
-
-    // Display result
-        if (isPalindrome) {
-        System.out.println(input + " is a Palindrome");
-    } else {
-        System.out.println(input + " is NOT a Palindrome");
-    }
-    String input = "madam";
-    String reversed = "";
-
-        for (int i = input.length() - 1; i >= 0; i--) {
-        reversed += input.charAt(i);
-    }
-
-        if (input.equals(reversed)) {
-        System.out.println(input + " is a palindrome");
-    } else {
-        System.out.println(input + " is not a palindrome");
 
 
+}
 
-    }
+
