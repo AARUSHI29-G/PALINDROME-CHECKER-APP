61216d5529319c09fc83539136804328518dba1b
AARUSHI29-G <aarushigullia@gmail.com>
2026-02-26 15:56:41 +0530

    UC2:Hardcoded palindrome

--- src/UseCase1PalindromeCheckerApp.java
+++ src/UseCase1PalindromeCheckerApp.java
@@ -10,6 +10,26 @@ public class UseCase1PalindromeCheckerApp {
         System.out.println("System initialized successfully.");
         System.out.println("-----------------------------------");
 
+        // Hardcoded string
+        String input = "madam";
 
+        boolean isPalindrome = true;
+
+        // Loop only till half of the string length
+        for (int i = 0; i < input.length() / 2; i++) {
+
+            // Compare characters from both ends
+            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        // Display result
+        if (isPalindrome) {
+            System.out.println(input + " is a Palindrome");
+        } else {
+            System.out.println(input + " is NOT a Palindrome");
+        }
     }
 }
